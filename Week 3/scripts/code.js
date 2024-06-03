ba4e19240286191aa63dd0fed2d6bf65d53db73f
EdwardMUAT <144376503+EdwardMUAT@users.noreply.github.com>
2024-06-02 19:48:58 -0700

    Add files via upload

---
+++ Week 3/scripts/code.js
@@ -0,0 +1,12 @@
+function setPageInfo(){
+    //add my name to the h1
+    document.getElementById("h1PageTitle").textContent = "Edward Mills Bio Info";
+    //add my major to the major div
+    document.getElementById("divMajor").textContent = "Major: Advancing Computer Science";
+    //add my email to the email div
+    document.getElementById("divEmail").textContent = "Email: Edwmills@uat.edu";
+    // add my grad date to div
+    document.getElementById("divGradDate").textContent  = "Expected Graduation Date: Spring 2027";
+
+    //console.log("")
+}
